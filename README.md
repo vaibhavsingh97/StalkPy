8891d0bda22814210e389ab79772006654757ad3
Vaibhav Singh <vaibhav.singh.14cse@bml.edu.in>
2017-08-27 01:41:44 +0530

    StalkPy beta v1.0

--- README.md
+++ README.md
@@ -1,5 +1,57 @@
 # StalkPy
-Command line tool to open the major social accounts connected to the specfic handle. Inspired from [HandleStalker](https://github.com/samanthakem/HandleStalker) :dancer: :rocket:  
 
-[![Build Status](https://travis-ci.org/vaibhavsingh97/StalkPy.svg?branch=master)](https://travis-ci.org/vaibhavsingh97/StalkPy) 
-[![Requirements Status](https://requires.io/github/vaibhavsingh97/StalkPy/requirements.svg?branch=master)](https://requires.io/github/vaibhavsingh97/StalkPy/requirements/?branch=master)
+Command line tool to open the major social accounts connected to the specfic handle. Inspired from [HandleStalker](https://github.com/samanthakem/HandleStalker) :dancer: :rocket:
+
+[![Build Status](https://travis-ci.org/vaibhavsingh97/StalkPy.svg?branch=master)](https://travis-ci.org/vaibhavsingh97/StalkPy) [![Requirements Status](https://requires.io/github/vaibhavsingh97/StalkPy/requirements.svg?branch=master)](https://requires.io/github/vaibhavsingh97/StalkPy/requirements/?branch=master) [![License: MIT](https://img.shields.io/badge/License-MIT-blue.svg)](https://vaibhavsingh97.mit-license.org/) <!-- https://github.com/jackton1/script_install -->
+
+```
+                     _____   _             _   _      _____          
+                    / ____| | |           | | | |    |  __ \         
+                    | (___  | |_    __ _  | | | | __ | |__) |  _   _
+                    \___ \  | __|  / _` | | | | |/ / |  ___/  | | | |
+                    ____) | | |_  | (_| | | | |   <  | |      | |_| |
+                    |_____/  \__|  \__,_| |_| |_|\_\ |_|       \__, |
+                                                                __/ |
+                                                               |___/
+```
+
+## Installation
+
+### Clone
+
+**1.)** Clone the repository by using this link :
+
+```
+https://github.com/vaibhavsingh97/StalkPy.git
+```
+
+### Run
+
+**2.)** Open terminal window there and the type these comands :
+
+```
+cd Stalkpy && pip install -r requirements.txt
+python StalkPy.py
+```
+
+### Configure
+
+**3.)** The `Config.json` contains a list of social accounts. You can add as many social accounts you want. Just remember to ensure the `Config.json` file is [valid JSON](http://jsonlint.com/).
+
+#### To-do
+
+- [ ] Make the script faster :)
+- [ ] check list of browser and open accordingly
+- [ ] Command line addition of accounts to `Config.json`
+- [ ] check the validity of username.
+- [ ] test cross plateform support
+
+## Issues
+
+You can report the bugs at the [issue tracker](https://github.com/vaibhavsingh97/StalkPy/issues)
+
+## License
+
+Built with ♥ by Vaibhav Singh([@vaibhavsingh97](https://github.com/vaibhavsingh97)) under [MIT License](https://vaibhavsingh97.mit-license.org/)
+
+You can find a copy of the License at <https://vaibhavsingh97.mit-license.org/>
--- StalkPy.py
+++ StalkPy.py
@@ -1,1 +1,124 @@
-print("StalkPy")
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#   _____   _             _   _      _____             __      __    __            ___
+#  / ____| | |           | | | |    |  __ \            \ \    / /   /_ |          / _ \
+# | (___   | |_    __ _  | | | | __ | |__) |  _   _     \ \  / /     | |         | | | |
+#  \___ \  | __|  / _` | | | | |/ / |  ___/  | | | |     \ \/ /      | |         | | | |
+#  ____) | | |_  | (_| | | | |   <  | |      | |_| |      \  /       | |    _    | |_| |
+# |_____/   \__|  \__,_| |_| |_|\_\ |_|       \__, |       \/        |_|   (_)    \___/
+#                                              __/ |
+#                                             |___/
+#-----------------------------------------------------------------------------
+# @Author: Vaibhav Singh
+# @Date: 2017-08-27
+# @Email: vaibhav.singh.14cse@bml.edu.in
+# @Github username: vaibhavsingh97
+# @Website: https://vaibhavsingh97.me
+# @Last Modified by: Vaibhav Singh
+# @Last Modified Date: 2017-08-27
+# @License: MIT License
+# you can find your copy of the License
+# https://vaibhavsingh97.mit-license.org/
+#-----------------------------------------------------------------------------
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import sys
+import json
+import requests
+import os
+import threading
+from clint.textui import colored, puts
+
+
+def head():
+    """official banner for StalkPy printed in green color"""
+    clear()
+    stalkPyBanner = """
+           _____   _             _   _      _____
+          / ____| | |           | | | |    |  __ \\
+         | (___   | |_    __ _  | | | | __ | |__) |  _   _
+          \___ \  | __|  / _` | | | | |/ / |  ___/  | | | |
+          ____) | | |_  | (_| | | | |   <  | |      | |_| |
+         |_____/   \__|  \__,_| |_| |_|\_\ |_|       \__, |
+                                                      __/ |
+                                                     |___/
+
+                                    - Vaibhav Singh (@ vaibhavsingh97)
+                                    - Version 1.0
+    """
+    puts(colored.green(stalkPyBanner))
+
+
+def clear():
+    """for removing the clutter from the screen when necessary"""
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def space():
+    """ for adding one linespace in the console"""
+    print("")
+
+
+def notification(flag):
+    if flag == 1:
+        puts(colored.red("Error: wrong value entered! Please enter correct value."))
+        space()
+        flag = 0
+    elif flag == 2:
+        puts(colored.green("Success: Social account found. :)"))
+        flag = 0
+    elif flag == 3:
+        puts(colored.red("No Social accounts found. :("))
+        flag = 0
+
+
+def showDoc():
+    documentation = """
+    A simple python script to open the major social accounts connected to the
+    specfic handle. Inspired from [HandleStalker](https://github.com/samanthakem/HandleStalker)
+
+    - Simple to use: Built with love so it's easy to use.
+    - single command will query any number of usernames
+    - Text Highlighting is cross platform - Supports Linux, MAC, Windows for the terminal based highlighting.
+    """
+    puts(colored.cyan(documentation))
+    space()
+
+
+def CleanedLink(url, username):
+    return "https://www." + url + username
+
+
+class StalkPy():
+    """docstring for ."""
+
+    def __init__(self):
+        with open('config.json') as data_file:
+            self.data = json.loads(data_file.read())
+
+    def OpenLinks(self, user):
+        for social_account_name in self.data:
+            r = requests.get(CleanedLink(self.data[social_account_name], user))
+            if r.status_code == 200:
+                notification(2)
+                driver = webdriver.Firefox()
+                driver.get(CleanedLink(self.data[social_account_name], user))
+                puts(colored.green(social_account_name + ": ") +
+                     colored.blue(CleanedLink(self.data[social_account_name], user)))
+            r.close()
+        notification(3)
+
+
+if __name__ == '__main__':
+    head()
+    showDoc()
+    if (len(sys.argv) < 2):
+        print("usage: \n python StalkPy.py <username1> <username2> ... <usernameN>")
+        puts(colored.red("Exiting..."))
+    else:
+        for i in range(1, len(sys.argv)):
+            space()
+            puts(colored.magenta("User: %s" % sys.argv[i]))
+            for t in range(10):
+                t = threading.Thread(target=StalkPy().OpenLinks(sys.argv[i]))
+                t.start()
